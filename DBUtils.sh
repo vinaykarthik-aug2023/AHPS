6a65d6bb7af36cb3d8f1e510347d77cb06e38da6
srilakshmi-2425 <srilakshmikintali2015@gmail.com>
2023-09-13 11:50:52 +0530

    updated

--- DBUtils.sh
+++ DBUtils.sh
@@ -4,4 +4,5 @@ i am updated this file
 i am updated this file in development branch
 Hello updated master branch
 Hello again updating in devbranch
-
+vinay is something added in development branch
+karthik is something added in master branch
